62b15ac54dcb19b517354722af8c669268a5b7c6
silverchris <silverchris@gmail.com>
2011-05-15 21:18:56 -0400

    fixed CPU and Network plugins

--- src/com/monitoring/munin_node/plugins/CPU.java
+++ src/com/monitoring/munin_node/plugins/CPU.java
@@ -6,14 +6,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 
 import com.monitoring.munin_node.plugin_api.Plugin_API;
 
-//TODO cleanup new lines so that there are no extras throught the output. 
 public class CPU implements Plugin_API{
+	ContextWrapper context = null;
 	public String getName(){
 		return "CPU";
 	}
@@ -21,13 +23,12 @@ public class CPU implements Plugin_API{
 		return "System";
 	}
 	@Override
-	public Boolean needsContext() {
-		// TODO Auto-generated method stub
-		return false;
+	public Boolean needsContext(){
+		return true;
 	}
 	@Override
-	public Void setContext(Context context) {
-		// TODO Auto-generated method stub
+	public Void setContext(Context newcontext) {
+		context = new ContextWrapper(newcontext);
 		return null;
 	}
 	@Override
@@ -62,7 +63,7 @@ public class CPU implements Plugin_API{
 		while (match.find()){
 			NCPU++;
 		}
-		int graphlimit = NCPU*100;
+		int graphlimit = 100;
 		output.append("graph_title CPU usage\n");
 		output.append("graph_order system user nice idle");
 		if(extinfo == true){
@@ -80,61 +81,106 @@ public class CPU implements Plugin_API{
 		output.append("system.label system\n");
 		output.append("system.draw AREA\n");
 		output.append("system.min 0\n");
-		output.append("system.type DERIVE\n");
 		output.append("system.info CPU time spent by the kernel in system activities\n");
 		output.append("user.label user\n");
 		output.append("user.draw STACK\n");
 		output.append("user.min 0\n");
-		output.append("user.type DERIVE\n");
 		output.append("user.info CPU time spent by normal programs and daemons\n");
 		output.append("nice.label nice\n");
 		output.append("nice.draw STACK\n");
 		output.append("nice.min 0\n");
-		output.append("nice.type DERIVE\n");
 		output.append("nice.info CPU time spent by nice(1)d programs\n");
 		output.append("idle.label idle\n");
 		output.append("idle.draw STACK\n");
 		output.append("idle.min 0\n");
-		output.append("idle.type DERIVE\n");
 		output.append("idle.info Idle CPU time");
 		if(extinfo == true){
 			output.append("\niowait.label iowait\n");
 			output.append("iowait.draw STACK\n");
 			output.append("iowait.min 0\n");
-			output.append("iowait.type DERIVE\n");
 			output.append("iowait.info CPU time spent waiting for I/O operations to finish when there is nothing else to do.\n");
 			output.append("irq.label irq\n");
 			output.append("irq.draw STACK\n");
 			output.append("irq.min 0\n");
-			output.append("irq.type DERIVE\n");
 			output.append("irq.info CPU time spent handling interrupts\n");
 			output.append("softirq.label softirq\n");
 			output.append("softirq.draw STACK\n");
 			output.append("softirq.min 0\n");
-			output.append("softirq.type DERIVE\n");
 			output.append("softirq.info CPU time spent handling batched interrupts");
 		}
 		if (extextinfo == true){
 			output.append("\nsteal.label steal\n");
 			output.append("steal.draw STACK\n");
 			output.append("steal.min 0\n");
-			output.append("steal.type DERIVE\n");
 			output.append("steal.info The time that a virtual CPU had runnable tasks, but the virtual CPU itself was not running");
 		}
+
 		StringBuffer output2 = new StringBuffer();
 		Pattern split_regex = Pattern.compile("\\s+");
 		String[] items = split_regex.split(statbuffer.toString());
-		output2.append("user.value "+items[1]+"\n");
-		output2.append("nice.value "+items[2]+"\n");
-		output2.append("system.value "+items[3]);
-		//output.append("idle.value "+items[4]);
+        final SharedPreferences settings = context.getSharedPreferences("Munin_Node.CPU", 0);
+        SharedPreferences.Editor editor = settings.edit();
+        Long oldUser = settings.getLong("user", 0);
+        Long oldNice = settings.getLong("nice", 0);
+        Long oldSystem = settings.getLong("system", 0);
+        Long oldIdle = settings.getLong("idle", 0);
+    	Long oldIowait = settings.getLong("iowait", 0);
+        Long oldIrq = settings.getLong("irq", 0);
+        Long oldSoftirq = settings.getLong("softirq",0);
+			Long oldSteal = settings.getLong("steal", 0);
+        editor.putLong("user", Long.parseLong(items[1]));
+        editor.putLong("nice", Long.parseLong(items[2]));
+        editor.putLong("system", Long.parseLong(items[3]));
+        editor.putLong("idle", Long.parseLong(items[4]));
 		if(extinfo == true){
-			output2.append("\niowait.value "+items[5]+"\n");
-			output2.append("irq.value "+items[6]+"\n");
-			output2.append("softirq.value "+items[7]);
+	        editor.putLong("iowait", Long.parseLong(items[5]));
+	        editor.putLong("irq", Long.parseLong(items[6]));
+	        editor.putLong("softirq", Long.parseLong(items[7]));
 		}
 		if(extextinfo == true){
-			output2.append("\nsteal.value "+items[8]);
+	        editor.putLong("steal", Long.parseLong(items[8]));
+		}
+        editor.commit();
+        Long Total = (Long.parseLong(items[1])-oldUser)+(Long.parseLong(items[2])-oldNice)+(Long.parseLong(items[3])-oldSystem)+(Long.parseLong(items[4])-oldIdle);
+        if(oldUser == 0){
+    		output2.append("user.value U\n");
+        }
+        else{
+        	Float user = new Float((100*(Long.parseLong(items[1])-oldUser))/new Float(Total));
+    		output2.append("user.value "+user.toString()+"\n");
+        }
+        if(oldNice == 0){
+    		output2.append("nice.value U\n");
+        }
+        else{
+        	Float nice = new Float((100*(Long.parseLong(items[2])-oldNice))/new Float(Total));
+    		output2.append("nice.value "+nice.toString()+"\n");
+        }
+        if(oldSystem == 0){
+    		output2.append("system.value U\n");
+        }
+        else{
+        	Float system = new Float((100*(Long.parseLong(items[3])-oldSystem))/new Float(Total));
+    		output2.append("system.value "+system.toString()+"\n");
+        }
+        if(oldIdle == 0){
+    		output2.append("idle.value U\n");
+        }
+        else{
+        	Float idle = new Float((100*(Long.parseLong(items[4])-oldIdle))/new Float(Total));
+    		output2.append("idle.value "+idle.toString()+"\n");
+        }
+		if(extinfo == true){
+	        	Float iowait = new Float((100*(Long.parseLong(items[5])-oldIowait))/new Float(Total));
+	        	Float irq = new Float((100*(Long.parseLong(items[6])-oldIrq))/new Float(Total));
+	        	Float softirq = new Float((100*(Long.parseLong(items[7])-oldSoftirq))/new Float(Total));
+				output2.append("\niowait.value "+iowait.toString()+"\n");
+				output2.append("irq.value "+irq.toString()+"\n");
+				output2.append("softirq.value "+softirq.toString());
+		}
+		if(extextinfo == true){
+	        	Float steal = new Float((100*(Long.parseLong(items[8])-oldSteal))/new Float(Total));
+	        	output2.append("\nsteal.value "+steal.toString());
 		}
 		Bundle bundle = new Bundle();
 		bundle.putString("name", this.getName());
--- src/com/monitoring/munin_node/plugins/network_traffic.java
+++ src/com/monitoring/munin_node/plugins/network_traffic.java
@@ -8,12 +8,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 
 import com.monitoring.munin_node.plugin_api.Plugin_API;
 
 public class network_traffic implements Plugin_API {
-
+	String[] Colours = {"00CC00", "0066B3", "FF8000", "FFCC00", "330099", "990099", "CCFF00", "FF0000", "808080", "008F00",
+			"00487D", "B35A00", "B38F00", "6B006B", "8FB300", "B30000", "BEBEBE", "80FF80", "80C9FF", "FFC080", "FFE680", 
+			"AA80FF", "EE00CC", "FF8080", "666600", "FFBFFF", "00FFCC", "CC6699", "999900"};
 	@Override
 	public String getName() {
 		return "Network Traffic";
@@ -24,84 +28,6 @@ public class network_traffic implements Plugin_API {
 		return "Network";
 	}
 
-	/*@Override
-	public String getConfig() {
-		StringBuffer output = new StringBuffer();
-		StringBuffer output2 = new StringBuffer();
-		output.append("graph_order down up\n"); 
-		output.append("graph_title Network traffic\n");
-		output.append("graph_args --base 1000\n");
-		output.append("graph_vlabel bits in (-) / out (+) per ${graph_period}\n");
-		output.append("graph_category network\n");
-		output.append("graph_info This graph shows the traffic of the network interfaces. Please note that the traffic is shown in bits per second, not bytes. IMPORTANT: On 32 bit systems the data source for this plugin uses 32bit counters, which makes the plugin unreliable and unsuitable for most 100Mb (or faster) interfaces, where traffic is expected to exceed 50Mbps over a 5 minute period.  This means that this plugin is unsuitable for most 32 bit production environments. To avoid this problem, use the ip_ plugin instead.  There should be no problems on 64 bit systems running 64 bit kernels.\n");
-		output2.append("\nmultigraph Network_Errors\n");
-		output2.append("graph_order rcvd trans\n");
-		output2.append("graph_title Network errors\n");
-		output2.append("graph_args --base 1000\n");
-		output2.append("graph_vlabel packets in (-) / out (+) per ${graph_period}\n");
-		output2.append("graph_category network\n");
-		output2.append("graph_info This graph shows the amount of errors on the network interfaces.\n");
-		Pattern network_pattern = Pattern.compile("([\\w\\d]+):\\s+([\\d]+)\\s+[\\d]+\\s+([\\d]+)\\s+[\\d]+\\s+\\s+[\\d]+\\s+[\\d]+\\s+[\\d]+\\s+[\\d]+\\s+([\\d]+)\\s+[\\d]+\\s+([\\d]+)\\s+");
-		
-		BufferedReader in = null;
-		try {
-			in = new BufferedReader(new FileReader("/proc/net/dev"));
-			String str;
-			while ((str = in.readLine()) != null) {
-				Matcher network_matcher = network_pattern.matcher(str);
-				if(network_matcher.find()){
-					output.append(network_matcher.group(1)+"down.label received\n");
-					output.append(network_matcher.group(1)+"down.type COUNTER\n");
-					output.append(network_matcher.group(1)+"down.graph no\n");
-					output.append(network_matcher.group(1)+"down.cdef "+network_matcher.group(1)+"down,8,*\n");
-					output.append(network_matcher.group(1)+"up.label bps\n");
-					output.append(network_matcher.group(1)+"up.type COUNTER\n");
-					output.append(network_matcher.group(1)+"up.negative "+network_matcher.group(1)+"down\n");
-					output.append(network_matcher.group(1)+"up.cdef "+network_matcher.group(1)+"up,8,*\n");
-					output2.append(network_matcher.group(1)+"rcvd.label packets\n");
-					output2.append(network_matcher.group(1)+"rcvd.type COUNTER\n");
-					output2.append(network_matcher.group(1)+"rcvd.graph no\n");
-					output2.append(network_matcher.group(1)+"rcvd.warning 1\n");
-					output2.append(network_matcher.group(1)+"trans.label packets\n");
-					output2.append(network_matcher.group(1)+"trans.type COUNTER\n");
-					output2.append(network_matcher.group(1)+"trans.negative "+network_matcher.group(1)+"rcvd\n");
-					output2.append(network_matcher.group(1)+"trans.warning 1\n");
-					}
-			}
-		} catch (FileNotFoundException e) {
-			return "";
-		} catch (IOException e) {
-			return "";
-		}
-		return output.toString()+output2.toString();
-	}
-
-	@Override
-	public String getUpdate() {
-		StringBuffer output = new StringBuffer();
-		StringBuffer output2 = new StringBuffer();
-		output2.append("\nmultigraph Network_Errors\n");
-		Pattern network_pattern = Pattern.compile("([\\w\\d]+):\\s+([\\d]+)\\s+[\\d]+\\s+([\\d]+)\\s+[\\d]+\\s+\\s+[\\d]+\\s+[\\d]+\\s+[\\d]+\\s+[\\d]+\\s+([\\d]+)\\s+[\\d]+\\s+([\\d]+)\\s+");
-		BufferedReader in = null;
-		try {
-			in = new BufferedReader(new FileReader("/proc/net/dev"));
-			String str;
-			while ((str = in.readLine()) != null) {
-				Matcher network_matcher = network_pattern.matcher(str);
-				if(network_matcher.find()){
-					output.append(network_matcher.group(1)+"down.value "+network_matcher.group(2)+"\n");
-					output.append(network_matcher.group(1)+"up.value "+network_matcher.group(4)+"\n");
-					output2.append(network_matcher.group(1)+"rcvd.value "+network_matcher.group(3)+"\n");
-					output2.append(network_matcher.group(1)+"trans.value "+network_matcher.group(5)+"\n");
-					}
-			}
-		} catch (FileNotFoundException e) {
-			return "";
-		} catch (IOException e) {
-			return "";
-		}
-		return output.toString()+output2.toString();
-	}*/
 
 	@Override
 	public Boolean needsContext() {
@@ -117,7 +43,73 @@ public class network_traffic implements Plugin_API {
 
 	@Override
 	public Void run(Handler handler) {
-		// TODO Auto-generated method stub
+		StringBuffer transconfig = new StringBuffer();
+		StringBuffer errorconfig = new StringBuffer();
+		StringBuffer transvalue = new StringBuffer();
+		StringBuffer errorvalue = new StringBuffer();
+		transconfig.append("graph_order down up\n"); 
+		transconfig.append("graph_title Network traffic\n");
+		transconfig.append("graph_args --base 1000\n");
+		transconfig.append("graph_vlabel bits in (-) / out (+) per ${graph_period}\n");
+		transconfig.append("graph_category network\n");
+		transconfig.append("graph_info This graph shows the traffic of the network interfaces. Please note that the traffic is shown in bits per second, not bytes. IMPORTANT: On 32 bit systems the data source for this plugin uses 32bit counters, which makes the plugin unreliable and unsuitable for most 100Mb (or faster) interfaces, where traffic is expected to exceed 50Mbps over a 5 minute period.  This means that this plugin is unsuitable for most 32 bit production environments. To avoid this problem, use the ip_ plugin instead.  There should be no problems on 64 bit systems running 64 bit kernels.\n");
+		errorconfig.append("\nmultigraph Network_Errors\n");
+		errorconfig.append("graph_order rcvd trans\n");
+		errorconfig.append("graph_title Network errors\n");
+		errorconfig.append("graph_args --base 1000\n");
+		errorconfig.append("graph_vlabel packets in (-) / out (+) per ${graph_period}\n");
+		errorconfig.append("graph_category network\n");
+		errorconfig.append("graph_info This graph shows the amount of errors on the network interfaces.\n");
+		Pattern network_pattern = Pattern.compile("([\\w\\d]+):\\s+([\\d]+)\\s+[\\d]+\\s+([\\d]+)\\s+[\\d]+\\s+\\s+[\\d]+\\s+[\\d]+\\s+[\\d]+\\s+[\\d]+\\s+([\\d]+)\\s+[\\d]+\\s+([\\d]+)\\s+");
+		
+		BufferedReader in = null;
+		try {
+			in = new BufferedReader(new FileReader("/proc/net/dev"));
+			String str;
+			Integer count = 0;
+			while ((str = in.readLine()) != null) {
+				Matcher network_matcher = network_pattern.matcher(str);
+				if(network_matcher.find()){
+					transconfig.append(network_matcher.group(1)+"down.label "+network_matcher.group(1)+" received\n");
+					transconfig.append(network_matcher.group(1)+"down.type COUNTER\n");
+					transconfig.append(network_matcher.group(1)+"down.graph yes\n");
+					transconfig.append(network_matcher.group(1)+"down.colour "+Colours[count]+"\n");
+					transconfig.append(network_matcher.group(1)+"down.cdef "+network_matcher.group(1)+"down,-8,*\n");
+					transconfig.append(network_matcher.group(1)+"up.label "+network_matcher.group(1)+" uploaded\n");
+					transconfig.append(network_matcher.group(1)+"up.type COUNTER\n");
+					transconfig.append(network_matcher.group(1)+"up.colour "+Colours[count]+"\n");
+					//transconfig.append(network_matcher.group(1)+"up.negative eth0down\n");
+					transconfig.append(network_matcher.group(1)+"up.cdef "+network_matcher.group(1)+"up,8,*\n");
+					errorconfig.append(network_matcher.group(1)+"rcvd.label "+network_matcher.group(1)+"\n");
+					errorconfig.append(network_matcher.group(1)+"rcvd.type COUNTER\n");
+					errorconfig.append(network_matcher.group(1)+"rcvd.graph yes\n");
+					errorconfig.append(network_matcher.group(1)+"rcvd.colour "+Colours[count]+"\n");
+					errorconfig.append(network_matcher.group(1)+"rcvd.warning 1\n");
+					errorconfig.append(network_matcher.group(1)+"trans.label "+network_matcher.group(1)+"\n");
+					errorconfig.append(network_matcher.group(1)+"trans.type COUNTER\n");
+					errorconfig.append(network_matcher.group(1)+"trans.colour "+Colours[count]+"\n");
+					//errorconfig.append(network_matcher.group(1)+"trans.negative "+network_matcher.group(1)+"rcvd\n");
+					errorconfig.append(network_matcher.group(1)+"trans.warning 1\n");
+					transvalue.append(network_matcher.group(1)+"down.value "+network_matcher.group(2)+"\n");
+					transvalue.append(network_matcher.group(1)+"up.value "+network_matcher.group(4)+"\n");
+					errorvalue.append(network_matcher.group(1)+"rcvd.value "+network_matcher.group(3)+"\n");
+					errorvalue.append(network_matcher.group(1)+"trans.value "+network_matcher.group(5)+"\n");
+					count++;
+					}
+			}
+		} catch (FileNotFoundException e) {
+			
+		} catch (IOException e) {
+			
+		}
+		Bundle bundle = new Bundle();
+		bundle.putString("name", this.getName());
+		//bundle.putString("config", transconfig.toString());
+		//bundle.putString("update", transvalue.toString());
+		bundle.putString("config", transconfig.toString()+errorconfig.toString());
+		bundle.putString("update", transvalue.toString()+errorvalue.toString());
+		Message msg = Message.obtain(handler, 42, bundle);
+		handler.sendMessage(msg);
 		return null;
 	}
 
