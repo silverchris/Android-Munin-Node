6f5858ece4c81ab597417720ad9229c4f219c00a
silverchris <silverchris@gmail.com>
2011-06-03 21:57:09 -0400

    Changing key for acra and google docs

--- src/com/monitoring/munin_node/MuninNode.java
+++ src/com/monitoring/munin_node/MuninNode.java
@@ -5,7 +5,7 @@ import org.acra.annotation.*;
 import android.app.Application;
 
 //@ReportsCrashes(formKey = "dHlEWldWSVdRQVhtWGZ5UTZKeUtvWHc6MQ")
-@ReportsCrashes(formKey = "dEFfUWlzMDNEaEZTQXgyek1yN291Unc6MQ")
+@ReportsCrashes(formKey = "dFNIMlVOdmVXM0loS1pBNGFJUmF3SFE6MQ")
 public class MuninNode extends Application {
 	   @Override
 	    public void onCreate() {
